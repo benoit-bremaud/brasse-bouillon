0a70bc401250a1468756bf3151a73977963a2067
benoit-bremaud <benoit.bremaud@laplateforme.io>
2025-04-22 16:48:27 +0200

    chore: update devDependencies for ESLint and related plugins

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,14 +1,6 @@
-// eslint.config.js
+// backend/eslint.config.js
 import js from '@eslint/js';
-import node from 'eslint-plugin-node';
 
 export default [
-  js.configs.recommended,
-  {
-    plugins: { node },
-    rules: {
-      'no-unused-vars': ['warn'],
-      'no-console': 'off'
-    },
-  },
+  js.configs.recommended
 ];

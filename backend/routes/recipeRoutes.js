4bdb29a248ea28fbf653fefc55833672b550a538
benoit-bremaud <benoit.bremaud@laplateforme.io>
2025-05-26 10:49:28 +0200

    feat(recipes): add PUT /recipes/:id endpoint to update a recipe

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -11,5 +11,11 @@ router.get('/', authenticateToken, recipeController.getAllRecipes);
 router.get('/:id', authenticateToken, recipeController.getRecipeById);
 // POST /recipes
 router.post('/', authenticateToken, recipeController.createRecipe);
+/**
+ * @route PUT /recipes/:id
+ * @desc Update a recipe (only allowed for the creator)
+ * @access Private (JWT required)
+ */
+router.put('/:id', authenticateToken, recipeController.updateRecipe);
 
 module.exports = router;

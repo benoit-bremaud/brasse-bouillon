986bd535457eb35d33ad018d92f9dd8c70be1f54
Benoît Bremaud <benoit.bremaud@vev.com>
2026-02-09 14:28:33 +0100

    build: setup eslint v9 flat config and jest expo tooling

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      'expo-router/babel',
       [
-        'module-resolver',
+        "module-resolver",
         {
           alias: {
-            '@': './src',
+            "@": "./src",
           },
         },
       ],
